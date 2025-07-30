2973759456b5b896ebaaa1edc4c4304fd2ccb40c
Adam Farhan <adamfarhan11@gmail.com>
2025-07-30 14:53:30 -0400

    set up inngest to handle basic AI jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,28 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { useTRPC } from "@/trpc/client";
 import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
+import { toast } from "sonner";
 
 const HomePage = () => {
+  const [value, setValue] = useState("");
   const trpc = useTRPC();
-  const invoke = useMutation(trpc.invoke.mutationOptions({}));
+  const invoke = useMutation(
+    trpc.invoke.mutationOptions({
+      onSuccess: () => {
+        toast.success("Background job started");
+      },
+    })
+  );
   return (
     <div className="p-4 max-w-7xl mx-auto">
+      <Input value={value} onChange={(e) => setValue(e.target.value)} />
       <Button
         onClick={() => {
-          invoke.mutate({ text: "hello" });
+          invoke.mutate({ value });
         }}
       >
         Invoke background job
